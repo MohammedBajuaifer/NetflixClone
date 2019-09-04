3c8ba60d2de5ef2f048d0f8e48441a620c6cd77b
Mohammed Salem <mohamadbajuaifer@gmail.com>
2019-09-04 21:05:50 +0300

    longer session time + handling Json better?

--- NetflixCustomAPI/netflix-api/api/controllers/netflix/home.js
+++ NetflixCustomAPI/netflix-api/api/controllers/netflix/home.js
@@ -7,7 +7,14 @@ module.exports = async function(req, res) {
 
 	const netflix = await Netflix.find({user: userId}).populate('user')
 
+	// console.log(req.body)
 	// return res.send(netflix)
-	res.view('pages/netflix/home', {netflix})
+
+	if (req.wantsJSON) {
+		res.send(netflix)
+	} else {
+		res.view('pages/netflix/home', {netflix})
+	}
+	// return res.send(netflix)
 
 }
